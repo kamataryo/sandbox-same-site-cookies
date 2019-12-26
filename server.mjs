4f0931efd186758edc155ea11da48e51f63cbdfc
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-12-26 12:14:26 +0900

    Start multi site

--- server.mjs
+++ server.mjs
@@ -26,20 +26,45 @@ const userTable = {
 const format = (text, vars) => {
   let result = text;
   Object.keys(vars).forEach(key => {
-    const tag = `%${key}`;
-    result = result.replace(tag, vars[key]);
+    const regexp = new RegExp(`%${key}`, "g");
+    result = result.replace(regexp, vars[key]);
   });
   return result;
 };
 
 const chars = "abcdef0123456789";
 
-const genSession = (username, n = 16) => {
+const websites = [
+  { port: 3000, sameSite: "Strict", siteName: "website A" },
+  { port: 4000, sameSite: "Lax", siteName: "website B" },
+  { port: 5000, sameSite: "None", siteName: "website C" }
+];
+
+const footerLinkList = `
+    <footer>
+      <h2>Switch server</h2>
+      <ul>${websites
+        .map(
+          ({ port, siteName, sameSite }) => `
+        <li>
+          <a href="http://localhost:${port}">
+            ${siteName}
+            <code>SameSite=${sameSite}</code>
+          </a>
+        </li>
+`
+        )
+        .join("")}</ul>
+    </footer>
+`;
+
+const genSession = (username, host, n = 16) => {
   if (userTable[username]) {
     const sessionId = Array.from(crypto.randomFillSync(new Uint8Array(n)))
       .map(n => chars[n % chars.length])
       .join("");
     userTable[username].sessionId = sessionId;
+    userTable[username].host = host;
     return sessionId;
   } else {
     return null;
@@ -52,13 +77,14 @@ const parseCookie = cookie => {
   );
 };
 
-const main = async () => {
+const main = async site => {
   const htmls = await getHTMLs([
     "home",
     "home-user",
     "login",
     "login-success",
     "login-failed",
+    "logout",
     "404",
     "405"
   ]);
@@ -79,20 +105,26 @@ const main = async () => {
             userTable[username] &&
             userTable[username].sessionId === sessionId
           ) {
-            res.write(format(htmls["home-user"], { username }));
+            res.write(
+              format(htmls["home-user"], {
+                username,
+                host: userTable[username].host,
+                ...site
+              })
+            );
             res.end();
           } else {
-            res.write(htmls.home);
+            res.write(format(htmls.home, site));
             res.end();
           }
         } else {
           res.statusCode = 405;
-          res.write(htmls[405]);
+          res.write(format(htmls[405], site));
           res.end();
         }
       } else if (url === "/login") {
         if (method === "GET") {
-          res.write(htmls.login);
+          res.write(format(htmls.login, site));
           res.end();
         } else if (method === "POST") {
           let body = [];
@@ -112,7 +144,7 @@ const main = async () => {
                 userTable[username] &&
                 userTable[username].password === password
               ) {
-                const sessionId = genSession(username);
+                const sessionId = genSession(username, req.headers.host);
                 const cookies = [
                   `Session-Id=${sessionId}; Max-Age=86400; Path=/`,
                   `Username=${username}; Max-Age=86400; Path=/`
@@ -121,41 +153,44 @@ const main = async () => {
                 res.setHeader("Set-Cookie", cookies);
                 console.log(res.getHeaders());
 
-                res.write(htmls["login-success"]);
+                res.write(format(htmls["login-success"], site));
                 res.end();
               } else {
-                res.write(htmls["login-success"]);
+                res.write(format(htmls["login-success"], site));
                 res.end();
               }
             });
         } else {
           res.statusCode = 405;
-          res.write(htmls[405]);
+          res.write(format(htmls[405], site));
           res.end();
         }
       } else if (url === "/logout") {
         if (method === "GET") {
           const { Username: username } = parseCookie(req.headers.cookie);
           delete userTable[username].sessionId;
+          delete userTable[username].host;
           const cookies = [
             `Session-Id=; Max-Age=0; Path=/`,
             `Username=; Max-Age=0; Path=/`
           ];
           res.setHeader("Set-Cookie", cookies);
-          res.write("successfully logged out!");
+          res.write(format(htmls.logout, site));
           res.end();
         } else {
           res.statusCode = 405;
-          res.write(htmls[405]);
+          res.write(format(htmls[405], site));
           res.end();
         }
       } else {
         res.statusCode = 404;
-        res.write(htmls[404]);
+        res.write(format(htmls[404], site));
         res.end();
       }
     })
-    .listen(3000, () => console.log("Listening..."));
+    .listen(site.port, () =>
+      console.log(`http://localhost:${site.port} is listening...`)
+    );
 };
 
-main();
+websites.forEach(site => main({ ...site, footerLinkList }));
